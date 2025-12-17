5ba66c6ecc1b26ba655a951a19a08400250d6702
neuro-tools-25 <jsdeveloper2021@yahoo.com>
2025-12-17 22:40:38 +0300

    Добавил запись в журнал изменений

--- src/views/ChangeLogView.vue
+++ src/views/ChangeLogView.vue
@@ -4,13 +4,25 @@
       <div v-if="isRus">
         <h1>Журнал изменений</h1>
 
-        <p>Искомая страница не найдена, попробуйте перейти по ссылкам ниже:</p>
+        <p>
+          В данном журнале я буду фиксировать изменения начиная с версии "0.2.0" и выше.
+        </p>
+
+        <p>
+          В версии "0.2.0" я добавил директиву "v-model:hovered-id". При наведении на маркированную область, через данную директиву я могу вывести её id.
+        </p>
       </div>
 
       <div v-if="isEng">
         <h1>Changelog</h1>
 
-        <p>The page you are looking for was not found, please try the links below:</p>
+        <p>
+I         In this log, I will record changes starting from version "0.2.0" and higher.
+        </p>
+
+        <p>
+          In version "0.2.0" I added the "v-model:hovered-id" directive. When hovering over a marked area, I can use this directive to output its id.
+        </p>
       </div>
     </animation-lay>
   </div>
